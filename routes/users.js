fb2996ee34ffefc0bfaf4fedfd7b754a76e0b485
ShotaMar <nu05kyntobronk@gmail.com>
2022-06-14 23:33:18 +0900

    ユーザ情報の削除機能実装 ユーザ情報の取得機能実装

--- routes/users.js
+++ routes/users.js
@@ -19,9 +19,31 @@ router.put('/:id', async(req, res) => {
     }
 })
 
-
 //ユーザ情報の削除
-//ユーザ情報の取得
+router.delete('/:id', async(req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            const user = await User.findByIdAndDelete(req.params.id)
+            res.status(200).json('ユーザー情報が削除されました')
 
+        }catch(err) {
+            return res.status(500).json(err)
+        }
+    }else{
+        return res.status(403).json('自分のアカウントの時だけ情報を削除できます')
+    }
+})
+
+//ユーザ情報の取得
+router.get('/:id', async(req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        const { password, updatedAt, ...others } = user._doc //_docは全てのプロパティ toJSON()と≒
+        return res.status(200).json(others)
+
+    }catch(err) {
+        return res.status(500).json(err)
+    }
+})
 
 module.exports = router
